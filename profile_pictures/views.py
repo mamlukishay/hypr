1648a84a5f45506f4460a09ef91b37bb9b5d7973
Ishay Mamluk <ishay@ourcrowd.com>
2018-06-10 23:14:13 +0300

    views.py a little DRYER

--- profile_pictures/views.py
+++ profile_pictures/views.py
@@ -1,14 +1,21 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound
 from profile_pictures.repositories import ProfilePicturesRepo
 from profile_pictures.interactions import store_profile_pic_for_twitter_handle
 
 def index(request):
   if request.method == 'GET':
-    handles = ProfilePicturesRepo().keys()
-    return JsonResponse({'handles': handles})
-  
-  if request.method == 'POST':
-    payload = json.loads(request.body)
-    profile_pic = store_profile_pic_for_twitter_handle.run(payload['handle'])
-    return JsonResponse({'profile_picture': profile_pic})
+    return list_handles()  
+  elif request.method == 'POST':
+    return create_profile_picture(request)
+  else:
+    return HttpResponseNotFound
+
+def create_profile_picture(request):
+  payload = json.loads(request.body)
+  profile_pic = store_profile_pic_for_twitter_handle.run(payload['handle'])
+  return JsonResponse({'profile_picture': profile_pic})
+
+def list_handles():
+  handles = ProfilePicturesRepo().keys()
+  return JsonResponse({'handles': handles})
