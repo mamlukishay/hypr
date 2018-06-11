36442db969afeb7ec5d01f292d61e04bb93ff689
Ishay Mamluk <ishay@ourcrowd.com>
2018-06-11 08:46:25 +0300

    added twitter_user class

--- services/twitter.py
+++
@@ -1,6 +0,0 @@
-import os.path
-
-TWITTER_BASE_URL = 'https://twitter.com'
-
-def profile_pic_url_for_handle(handle):
-  return os.path.join(TWITTER_BASE_URL, handle, 'photo')
---
+++ social_providers/twitter.py
@@ -0,0 +1,10 @@
+import os.path
+
+TWITTER_BASE_URL = 'https://twitter.com'
+
+class TwitterUser(object):
+  def __init__(self, handle):
+    self.handle = handle
+
+  def profile_picture(self):
+    return os.path.join(TWITTER_BASE_URL, self.handle, 'photo')
